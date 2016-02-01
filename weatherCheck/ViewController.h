b690e16954d33c53d96e572f2678c6a111d360bd
iMac2 <imac2@imac2.odc.localdomain>
2016-02-01 12:15:04 +0530

    Initial Commit

---
+++ weatherCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weatherCheck
+//
+//  Created by iMac2 on 01/02/16.
+//  Copyright © 2016 SJI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weatherCheck/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weatherCheck
+//
+//  Created by iMac2 on 01/02/16.
+//  Copyright © 2016 SJI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
